4d6adc802fda785f8bbbe272aa3c6ffd987df175
Francesca Polzella <franpolzella@gmail.com>
2024-10-12 15:46:49 -0400

    nuevo

---
+++ js/db.js
@@ -0,0 +1,162 @@
+const Libros = [
+	{
+		ISBN: '978-8498387087',
+		Titulo: 'El jilguero',
+		year: 2014,
+		Categoria: 'Ficción contemporánea',
+		Autor: 'Donna Tartt',
+		
+	},
+	{ 
+		ISBN: '978-0062315007 ',
+		Titulo: 'El alquimista',
+		year: 2015 ,
+		Categoria: ' Ficción, Filosofía',
+		Autor: 'Paulo Coelho',
+		
+    },
+	{
+		ISBN: ' 978-0307387899',
+		Titulo: 'La carretera',
+		year: 2016 ,
+		Categoria: ' Ficción post-apocalíptica',
+		Autor: 'Cormac McCarthy',
+		
+	},
+	{ 
+		ISBN: ' 978-8498387100',
+		Titulo: 'Cometas en el cielo',
+		year:2017 ,
+		Categoria: 'Ficción histórica',
+		Autor:'Khaled Hosseini'
+    },
+	{
+		ISBN: ' 978-8498387101',
+		Titulo: 'La vida de Pi',
+		year:2018 ,
+		Categoria: ' Aventura, Ficción',
+		Autor:'Yann Martel'
+	},
+	{
+		
+		ISBN: '978-8498387102 ',
+		Titulo: 'El circo de la noche',
+		year:2019 ,
+		Categoria: 'Fantasía',
+		Autor:'Erin Morgenstern'
+	},
+	
+	{
+		
+		ISBN: ' 978-8498387103',
+		Titulo: 'Matar a un ruiseñor',
+		year:2020 ,
+		Categoria: 'Clásico, Ficción',
+		Autor:'Harper Lee'
+		
+	},
+	{ 
+       
+		ISBN: ' 978-8498387104',
+		Titulo: 'La ladrona de libros',
+		year:2021 ,
+		Categoria: ' Ficción histórica',
+		Autor: 'Markus Zusak'
+    },
+	{
+		
+		ISBN: '  978-8498387105',
+		Titulo: 'Desde mi cielo',
+		year: 2022,
+		Categoria: ' Ficción, Misterio',
+		Autor:'Alice Sebold'
+	},
+	{ 
+        
+		ISBN: '978-8498387106 ',
+		Titulo: '"La luz entre los océanos',
+		year: 2023,
+		Categoria: ' Ficción histórica',
+		Autor:'M.L. Stedman'
+    },
+	{
+		
+		ISBN: ' 978-8498387107 ',
+		Titulo: 'El océano al final del camino',
+		year:2014 ,
+		Categoria: 'Fantasía',
+		Autor:'Neil Gaiman'
+	},
+	{
+		
+		ISBN: ' 978-8498387108 ',
+		Titulo: 'La verdad sobre el caso Harry Quebert',
+		year: 2015,
+		Categoria: 'Misterio',
+		Autor:' Joël Dicker'
+	},
+	{
+		
+		ISBN: ' 978-8498387109 ',
+		Titulo: 'Bajo la misma estrella',
+		year: 2016,
+		Categoria: ' Ficción juvenil',
+		Autor:'John Green'
+	},
+	{ 
+        
+		ISBN: ' 978-8498387110 ',
+		Titulo: 'Nos vemos allá arriba',
+		year: 2017,
+		Categoria: 'Ficción histórica',
+		Autor:'Pierre Lemaitre'
+    },
+	{
+		
+		ISBN: '978-8498387111',
+		Titulo: 'En la orilla',
+		year:2018 ,
+		Categoria: ' Ficción contemporánea',
+		Autor:'Rafael Chirbes'
+	},
+	{
+		
+		ISBN: '978-8498387112 ',
+		Titulo: 'Sapiens: De animales a dioses',
+		year: 2019,
+		Categoria: ' Historia, Ciencia',
+		Autor:'Yuval Noah Harari'
+	},
+	{
+		
+		ISBN: '978-8498387113 ',
+		Titulo: 'Patria',
+		year: 2020,
+		Categoria: 'Ficción contemporánea',
+		Autor:'Fernando Aramburu'
+	},
+	{
+		
+		ISBN: ' 978-8498387114',
+		Titulo: 'El hombre que amaba a los perros',
+		year:2021 ,
+		Categoria: 'Ficción histórica',
+		Autor:'Leonardo Padura'
+	},
+	{ 
+      
+		ISBN: '978-8498387115 ',
+		Titulo: 'El tiempo entre costuras',
+		year:2022 ,
+		Categoria: ' Ficción histórica',
+		Autor:'María Dueñas'
+    }
+	{ 
+      
+		ISBN: ' 978-8498387116',
+		Titulo: 'La sombra del viento',
+		year: 2023,
+		Categoria: 'Misterio, Ficción histórica',
+		Autor:' Carlos Ruiz Zafón'
+    }
+];
